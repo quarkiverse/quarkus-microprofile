0ed5992f77f13cf44dd74ea141f551d46439e3d4
Roberto Cortez <radcortez@yahoo.com>
2024-02-09 08:05:42 +0000

    Replace OpenTracing by OpenTelemetry (#134)

---
+++ platform/deployment/src/main/java/io/quarkiverse/microprofile/telemetry/TelemetryProcessor.java
@@ -0,0 +1,12 @@
+package io.quarkiverse.microprofile.telemetry;
+
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.annotations.BuildProducer;
+import io.quarkus.deployment.annotations.BuildStep;
+
+class TelemetryProcessor {
+    @BuildStep
+    void additionalBeans(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
+        additionalBeans.produce(new AdditionalBeanBuildItem(TelemetryProducer.class));
+    }
+}
